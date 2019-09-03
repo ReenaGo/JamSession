53f4abd3c451c081605c06b9eb6990ea81a32334
Alan Klein <aklein@osisecure.com>
2019-09-03 16:42:10 -0400

    Add main page redirect to signup

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV !== "production") {
 // Load configuration information from system environment variables.
 const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
 const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
-const TWILIO_CHAT_SERVICE_SID = process.env.TWILIO_CHAT_SERVICE_SID ;
+const TWILIO_CHAT_SERVICE_SID = process.env.TWILIO_CHAT_SERVICE_SID;
 const TWILIO_API_KEY = process.env.TWILIO_API_KEY;
 const TWILIO_API_SECRET = process.env.TWILIO_API_SECRET;
 const PORT = process.env.PORT || 3000;
@@ -56,6 +56,12 @@ app.use(express.static(path.join(__dirname, "assets")));
 app.set("view engine", "ejs");
 app.set("views", "app/views");
 
+// Redirect to Signup page when visiting main URL
+
+app.get("/", function(req, res, next) {
+  res.redirect("/signup");
+});
+
 //***********************************************************************************************/
 //Signup functionality
 app.get("/signup", (req, res, next) => {
@@ -76,7 +82,7 @@ app.post("/signup", (req, res, next) => {
       .create({ firstName: firstName, lastName: lastName, email: email, Image: imageURL, Bio: bio, password: hash })
       .then(user => {
         req.session.user_id = user.id;
-        req.session.firstName= firstName;
+        req.session.firstName = firstName;
         res.redirect("/profilePage");
       });
   });
@@ -108,49 +114,45 @@ app.post("/login", (req, res, next) => {
 
 // ************************************************************************************************
 // Profile Page Functionality
-app.get("/profilePage",(req,res,next)=>{
-    // res.render('profilePage')
-      db.user.findByPk(req.session.user_id).then(function (user) {
-        db.communities.findAll().then(function (communities){
-        res.render('profilePage', {
-            firstName: user.firstName,
-            lastName: user.lastName,
-            imageURL: user.Image,
-            bio: user.Bio,
-            communities: communities
-          });
-
-        });
-        //console.log(communities)
+app.get("/profilePage", (req, res, next) => {
+  // res.render('profilePage')
+  db.user.findByPk(req.session.user_id).then(function(user) {
+    db.communities.findAll().then(function(communities) {
+      res.render("profilePage", {
+        firstName: user.firstName,
+        lastName: user.lastName,
+        imageURL: user.Image,
+        bio: user.Bio,
+        communities: communities
       });
+    });
+    //console.log(communities)
   });
-
-
+});
 
 // *************************************************************************************************
 // community Page functionality
-app.get('/community/:id', (req, res, next)=>{
+app.get("/community/:id", (req, res, next) => {
   let communityId = req.params.id;
   let userId = req.session.user_id;
   let firstName = req.session.firstName;
-   req.session.community_id = communityId;
-  db.communities.findOne({ where: {id: communityId } }).then(function (communities) {
-    db.jam.findAll().then(function(jams){
-    res.render('community', {
-      comName: communities.comName,
-      description: communities.Description,
-      jams:jams,
-      userId: userId,
-      firstName: firstName
+  req.session.community_id = communityId;
+  db.communities.findOne({ where: { id: communityId } }).then(function(communities) {
+    db.jam.findAll().then(function(jams) {
+      res.render("community", {
+        comName: communities.comName,
+        description: communities.Description,
+        jams: jams,
+        userId: userId,
+        firstName: firstName
       });
     });
   });
 });
 
-
 //**************************************************************************************************/
 // community Page create Jams
-app.post('/community/:id',(req,res,next)=>{
+app.post("/community/:id", (req, res, next) => {
   let communityId = req.params.id;
   req.session.community_id = communityId;
 
@@ -159,38 +161,29 @@ app.post('/community/:id',(req,res,next)=>{
   date = req.body.date;
   time = req.body.time;
 
-  db.jam.create({jamName: jamName, jamDescription: jamDescription, date: date, time: time}).then((jam)=>{
+  db.jam.create({ jamName: jamName, jamDescription: jamDescription, date: date, time: time }).then(jam => {
     res.redirect(`/community/${communityId}`);
+  });
 });
-
-
-})
-
 
 //**********************************************************************************************/
 //logout
-app.get("/signOut", function (req, res, next) {
-  req.session.destroy(function () {
+app.get("/signOut", function(req, res, next) {
+  req.session.destroy(function() {
     res.redirect("/signup");
   });
 });
 
 //**********************************************************************************************/
 //video route
-app.get("/video/:jamName", function (req, res, next) {
-
+app.get("/video/:jamName", function(req, res, next) {
   let firstName = req.session.firstName;
   let jamSession = req.params.jamName;
-  res.render('video', {
+  res.render("video", {
     jamSession: jamSession,
     firstName: firstName
-    });
-
+  });
 });
-
-
-
-
 
 // endpoint to procure Twilio Video Token
 app.get("/videoToken", (req, res) => {
@@ -225,18 +218,13 @@ parameter.
 
 app.get("/chatToken", function(req, res) {
   let username = req.query.username;
-    console.log("username is: ", username);
-  let token = new AccessToken(
-      TWILIO_ACCOUNT_SID,
-      TWILIO_API_KEY,
-      TWILIO_API_SECRET,
-      {
-          identity: username,
-          ttl: 40000
-      }
-  );
+  console.log("username is: ", username);
+  let token = new AccessToken(TWILIO_ACCOUNT_SID, TWILIO_API_KEY, TWILIO_API_SECRET, {
+    identity: username,
+    ttl: 40000
+  });
 
-let grant = new ChatGrant({ serviceSid: TWILIO_CHAT_SERVICE_SID });
+  let grant = new ChatGrant({ serviceSid: TWILIO_CHAT_SERVICE_SID });
 
   token.addGrant(grant);
   const tokenJwt = token.toJwt();
